7d97df6c0e02bfd921fc0211bdf9aa773cc064b0
soumya shekhar <soumyashekhar.dtu27@gmail.com>
2025-06-22 17:05:40 +0530

    1

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async ()=>{
     try {
         mongoose.connection.on('connected', ()=> console.log("DB Connected"))
-        await mongoose.connect(`${process.env.MONGO_URI}/FULLSTACK`)
+        await mongoose.connect(`${process.env.MONGO_URI}/Project 0`)
 
     } catch(error){
             console.log(error.message);
